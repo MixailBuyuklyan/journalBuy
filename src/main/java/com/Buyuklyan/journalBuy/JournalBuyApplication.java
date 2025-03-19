00d0053284db147454d4cbff0195f0acca9d301d
MixailBuyuklyan <buyuklyan03@yandex.ru>
2025-03-19 19:14:55 +0300

    2 Создание  простого Web приложения АСЗИ

---
+++ src/main/java/com/Buyuklyan/journalBuy/JournalBuyApplication.java
@@ -0,0 +1,13 @@
+package com.Buyuklyan.journalBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Buyuklyan/journalBuy/JournalBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Buyuklyan.journalBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
